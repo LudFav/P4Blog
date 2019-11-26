720c1d5d6027585dfed711a592035463ba614b73
LudovicFavory <lu.favory@gmail.com>
2019-11-26 15:27:08 +0100

    Update Router.php

--- app/controllers/Router.php
+++ app/controllers/Router.php
@@ -16,9 +16,24 @@ class Router {
             if(isset($_GET['url'])){
                 // on decompose l'url et on lui applique le filtre FILTER_SANITIZE_URL qui supprime tous les caractères d'URL illégales d'une chaîne 
                 $url = explode('/', filter_var($_GET['url'], FILTER_SANITIZE_URL))
-            }
-        } catch(){
+                $controller = ucfirst(strtolower($url[0]));// on veille à ne mettre que la premiere lettre de notre 1er item d'url en majuscule.
+                $controllerClass = "Controller".$controller;
+                $controllerFile = "controllers/".$controllerClass.".php";
 
+                //verification que le fichier du controleur existe
+                if(file_exists($controllerFile)){
+                    require_once($controllerFile);
+                    $this->ctrl = new $controllerClass($url)
+                } else {
+                    throw new \Exception("Page introuvable")
+                }
+            } else {
+                require_once('controllers/ControllerHome.php')
+                $this->ctrl = new ControllerHome($url)
+            }
+        } catch(\Exception $e){
+            $errorMessage = $e->getMessage();
+            require_once('views/viewError.php');
         }
     }
 }
