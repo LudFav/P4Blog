4bc34113716be8f216800870eec4c0f59e5b6aa5
LudovicFavory <lu.favory@gmail.com>
2019-11-27 16:31:08 +0100

    Update HomeController.php

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -1,5 +1,19 @@
 <?php
 
 class HomeController{
-    
+    private = $_billetManager;
+    private $_view;
+    public function __construct($url){
+        if(isset($url) && count($urm)> 1){
+            throw new \Exception("Page introuvable", 1);
+        } else {
+            $this->billets();
+        }
+    }
+
+    private function billets(){
+        $this->_billetManager = new BilletManager();
+        $billets = $this->billetManager->getBillet();
+        require_once('HomeView.php'); // à securiser
+    }
 }
