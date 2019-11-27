af2359822974a6dd2f5e258d3b1577b183653222
LudovicFavory <lu.favory@gmail.com>
2019-11-27 15:00:45 +0100

    Create Model.php

---
+++ models/Model.php
@@ -0,0 +1,37 @@
+<?php 
+abstract class Model{
+    private static $_bdd;
+
+    // connexion a la base de donnée
+    private static function setBdd(){
+        self::$_bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+        //utilisation des const de PDO pour gerer les erreurs 
+        self::$_bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING)
+    }
+
+    //methode de connexion par defaut à la base de donnée
+    protected function getBdd(){
+        if(self::$_bdd == null){
+            self::setBdd();
+            return self::$_bdd;
+        }
+    }
+
+    //methode de recuperation de la liste des elements de la BDD
+    protected function getAll($table, $obj){
+        $this->getBdd();
+        $arrayTable  = [];
+        $requete = self::$_bdd->prepare('SELECT * FROM' .$table.'ORDER BY id DESC')
+        $requete->execute();
+
+        // creation de la variable data contenant les données recuperées de la table cible
+        while($data = $requete->fetch(PDO::FETCH_ASSOC)){
+            //$arrayTable contiendra les données sous forme d'objets
+            $arrayTable[]= new $obj($data);
+        }
+
+        return $arrayTable;
+        $requete->closeCursor();
+
+    }
+}
