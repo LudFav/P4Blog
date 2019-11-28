d7a1b348cf3085dba58469f67f61610c568e978e
LudovicFavory <lu.favory@gmail.com>
2019-11-28 14:32:38 +0100

    Update Model.php

--- models/Model.php
+++ models/Model.php
@@ -1,37 +1,84 @@
-<?php 
-abstract class Model{
-    private static $_bdd;
+<?php
 
-    // connexion a la base de donnée
-    private static function setBdd(){
-        self::$_bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
-        //utilisation des const de PDO pour gerer les erreurs 
-        self::$_bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+abstract class Model
+{
+
+  private static $_bdd;
+
+  //connexion a la bdd
+
+  private static function setBdd(){
+    self::$_bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+
+    //on utilise les constantes de PDO pour gérer les erreurs
+    self::$_bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+  }
+
+  //fonction de connexion par defaut a la bdd
+  protected function getBdd(){
+    if (self::$_bdd == null) {
+      self::setBdd();
+      return self::$_bdd;
+    }
+  }
+
+  //creation d ela methode
+  //de récupération de liste d'elements
+  //dans la bdd
+
+  protected function getAll($table, $obj){
+    $this->getBdd();
+    $var = [];
+    $req = self::$_bdd->prepare('SELECT * FROM '.$table.' ORDER BY id desc');
+    $req->execute();
+
+    //on crée la variable data qui
+    //va cobntenir les données
+    while ($data = $req->fetch(PDO::FETCH_ASSOC)) {
+      // var contiendra les données sous forme d'objets
+      $var[] = new $obj($data);
     }
 
-    //methode de connexion par defaut à la base de donnée
-    protected function getBdd(){
-        if(self::$_bdd == null){
-            self::setBdd();
-            return self::$_bdd;
-        }
+    return $var;
+    $req->closeCursor();
+
+
+  }
+
+  protected function getOne($table, $obj, $id)
+  {
+    $this->getBdd();
+    $var = [];
+    $req = self::$_bdd->prepare("SELECT id, auteur, titre, contenu, DATE_FORMAT(date, '%d/%m/%Y à %Hh%i') AS date FROM " .$table. " WHERE id = ?");
+    $req->execute(array($id));
+    while ($data = $req->fetch(PDO::FETCH_ASSOC)) {
+      $var[] = new $obj($data);
     }
 
-    //methode de recuperation de la liste des elements de la BDD
-    protected function getAll($table, $obj){
-        $this->getBdd();
-        $arrayTable  = [];
-        $requete = self::$_bdd->prepare(' SELECT * FROM ' .$table. ' ORDER BY id desc');
-        $requete->execute();
+    return $var;
+    $req->closeCursor();
+  }
 
-        // creation de la variable data contenant les données recuperées de la table cible
-        while($data = $requete->fetch(PDO::FETCH_ASSOC)){
-            //$arrayTable contiendra les données sous forme d'objets
-            $arrayTable[]= new $obj($data);
-        }
 
-        return $arrayTable;
-        $requete->closeCursor();
 
-    }
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+ ?>
