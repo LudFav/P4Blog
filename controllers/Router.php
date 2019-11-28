b7f43013ba2046d6564a230d5b1539d1a0decf44
LudovicFavory <lu.favory@gmail.com>
2019-11-28 14:32:28 +0100

    Update Router.php

--- controllers/Router.php
+++ controllers/Router.php
@@ -1,40 +1,89 @@
-<?php 
-class Router {
-    private $ctrl;
-    private $view;
+<?php
+require_once 'views/View.php';
+/**
+ *
+ */
+class Router
+{
+  private $ctrl;
+  private $view;
 
-    public function routeUrl(){
-        try {
-            //chargement automatique des classes du dossier models
-            spl_autoload_register(function($class){
-                require_once('models/'.$class.'.php');
-            });
-            // creation de la variable $url
-            $url='';
-            // determine le controller en fonction de la valeur de cette variable
-            if(isset($_GET['url'])){
-                // on decompose l'url grace a explode et on lui applique un filtre de securité, n'acceptant que les caracteres legaux d'URL
-                $url = explode('/', filter_var($_GET['url'], FILTER_SANITIZE_URL));
-                // recuperation du premier item de l'url, on s'assure que seule la premiere lettre est en majuscule
-                $controller = ucfirst(strtolower($url[0]));
-                $controllerClass = "Controller".$controller;
-                //chemin du controller voulu
-                $controllerFile = "controllers/".$controllerClass.".php";
+  public function routeReq(){
 
-                if(file_exists($controllerFile)){
-                    //si $controllerFile existe on lance la class
-                    require_once($controllerFile);
-                    $this->ctrl = new $controllerClass($url);
-                } else {
-                    throw new \Exception ("Page Introuvable", 1);
-                }
-            } else {
-                require_once('controllers/HomeController.php');
-                $this->ctrl = new HomeController($url);
-            }
-        } catch(\Exception $e){
-            $errorMsf = $e->getMessage();
-            require_once('views/ErrorView.php');// pour le moment pas assez securisé
+    try {
+
+      //chargement automatique des classes du dossier models
+      spl_autoload_register(function($class){
+        require_once('models/'.$class.'.php');
+      });
+
+      //on crée une variable $url
+      $url = '';
+
+      //on va determiner le controleur en
+      //fonction de la valeur de cette variable
+      if (isset($_GET['url'])) {
+        //on décompose l'url et on lui applique un filtre
+        $url = explode('/', filter_var($_GET['url'], FILTER_SANITIZE_URL));
+
+        //on recupere le premier parametre de url
+        //on le met tout en miniscule
+        //on met sa premiere lettre en majuscule
+        $controller = ucfirst(strtolower($url[0]));
+
+        $controllerClass = "Controller".$controller;
+
+        //on retrouve le chemin du controleur voulu
+        $controllerFile = "controllers/".$controllerClass.".php";
+
+        //on check si le fichier du controleur existe
+        if (file_exists($controllerFile)) {
+          //on lance la classe en question
+          //avec tous les parametres url
+          //pour respecter l'encapsulation
+          require_once($controllerFile);
+          $this->ctrl = new $controllerClass($url);
         }
+        else {
+          throw new \Exception("Page introuvable", 1);
+
+        }
+      }
+
+      else {
+        require_once('controllers/ControllerAccueil.php');
+        $this->ctrl = new ControllerAccueil($url);
+      }
+
+    } catch (\Exception $e) {
+      $errorMsg = $e->getMessage();
+      $this->_view = new View('Error');
+      $this->_view->generate(array('errorMsg' => $errorMsg));
     }
+
+
+
+
+
+
+
+  }
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
