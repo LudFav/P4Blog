75e57071ed6ed180cb2659c9bbf0bfe4de1c88b0
LudovicFavory <lu.favory@gmail.com>
2019-11-28 14:32:21 +0100

    Create ControllerPost.php

---
+++ controllers/ControllerPost.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'views/View.php';
+
+class ControllerPost
+
+{
+  private $_billetManager;
+  private $_view;
+
+  public function __construct()
+  {
+    if (isset($url) && count($url) < 1) {
+      throw new \Exception("Page Introuvable");
+    } else {
+      $this->billet();
+    }
+  }
+
+  private function billet()
+  {
+    if (isset($_GET['id'])) {
+      $this->_billetManager = new BilletManager;
+      $billet = $this->_billetManager->getBillet($_GET['id']);
+      $this->_view = new View('SinglePost');
+      $this->_view->generate(array('billet' => $billet));
+    }
+
+  }
+}
+
+ ?>
