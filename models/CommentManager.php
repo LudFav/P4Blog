34fc1e2f535f14efe7d0a18702dd3931d42acae8
LudovicFavory <lu.favory@gmail.com>
2019-12-05 11:36:00 +0100

    Update CommentManager.php

--- models/CommentManager.php
+++ models/CommentManager.php
@@ -5,10 +5,10 @@ class CommentManager extends Model {
   //gérer la fonction qui va recuperer
   //tous les commentaire dans la bdd
   public function getComments(){
-    return $this->getAll('commentaire', 'comment');
+    return $this->getAll('commentaire', 'Comment');
   }
 
   public function getComment($id){
-      return $this->getOne('commentaire', 'comment', $id);
+      return $this->getOne('commentaire', 'Comment', $id);
   }
 }
