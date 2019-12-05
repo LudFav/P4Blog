780cfb05c01303cea41a7982b10b37710a51cce1
LudovicFavory <lu.favory@gmail.com>
2019-12-05 10:11:08 +0100

    Update ControllerPost.php

--- controllers/ControllerPost.php
+++ controllers/ControllerPost.php
@@ -1,14 +1,11 @@
 <?php
 require_once 'views/View.php';
 
-class ControllerPost
-
-{
+class ControllerPost {
   private $_billetManager;
   private $_view;
 
-  public function __construct()
-  {
+  public function __construct(){
     if (isset($url) && count($url) < 1) {
       throw new \Exception("Page Introuvable");
     } else {
@@ -16,14 +13,17 @@ class ControllerPost
     }
   }
 
-  private function billet()
-  {
+  private function billet(){
     if (isset($_GET['id'])) {
       $this->_billetManager = new BilletManager;
       $billet = $this->_billetManager->getBillet($_GET['id']);
       $this->_view = new View('SinglePost');
       $this->_view->generate(array('billet' => $billet));
     }
+  }
+
+  private function comment(){
+    if (isset($_GET[''])){}
   }
 }
 
