a4c12dd506ef8d788fbdd1498c0717bdcd00e8c0
LudovicFavory <lu.favory@gmail.com>
2019-12-10 11:04:06 +0100

    Update ControllerPost.php

--- controllers/ControllerPost.php
+++ controllers/ControllerPost.php
@@ -10,11 +10,27 @@ class ControllerPost {
     if (isset($url) && count($url) < 1) {
       throw new \Exception("Page Introuvable");
     } else {
-      $this->billet();
-      $this->comment();
+      $this->post();
+      //$this->billet();
+      //$this->commentaires();
+     
     }
   }
 
+  private function post(){
+    $this->_view = new View('SinglePost');
+    if (isset($_GET['id'])) {
+      $this->_billetManager = new BilletManager;
+      $billet = $this->_billetManager->getBillet($_GET['id']);
+      $this->_view->generate(array('billet' => $billet));
+    }
+
+      $this->_commentManager = new CommentManager;
+      $comments = $this->_commentManager->getComments();
+      $this->_view->generate(array('comments' => $comments));
+    
+  }
+/* 
   private function billet(){
     if (isset($_GET['id'])) {
       $this->_billetManager = new BilletManager;
@@ -24,13 +40,14 @@ class ControllerPost {
     }
   }
 
-  private function comment(){
-    if (isset($_GET['id'])){
+  private function commentaires(){
+    if (isset($_GET['billetId'])){
       $this->_commentManager = new CommentManager;
-      $comment = $this->_commentManager->getComments($_GET['billet']);
-      $this->_view = new View('SinglePost');
-      $this->_view->generate(array('comment' => $comment));
+      $comment = $this->_commentManager->getComments($_GET['billetId']);
+      $this->_view->generate(array('commentaires' => $commentaires));
     }
   }
+
+*/
 }
 
