f7f287cf129cabb30ab86670d5e8e4630cf08cb9
LudovicFavory <lu.favory@gmail.com>
2019-12-12 16:51:06 +0100

    Update Comment.php

--- models/Comment.php
+++ models/Comment.php
@@ -15,11 +15,11 @@ class Comment
     }
 
     public function hydrate(array $data){
-        foreach($data as $key => $value){
-            $method = 'set'.ucfirst($key) 
-            if ( method_exists($this, $method)){
-                $this->method($value);
-            }
+        foreach ($data as $key => $value) {
+          $method = 'set'.ucfirst($key);
+          if (method_exists($this, $method)) {
+            $this->$method($value);
+          }
         }
     }
 //setters 
