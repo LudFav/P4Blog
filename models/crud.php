7e55502657d45f51a991b84dd81fc0d1f26178cf
LudovicFavory <lu.favory@gmail.com>
2019-12-17 14:25:26 +0100

    Update crud.php

--- models/crud.php
+++ models/crud.php
@@ -4,6 +4,10 @@ interface crud {
 
     public function create();
 
+    public function readAll();
+
+    public function readOne();
+
     public function update();
 
     public function delete();
