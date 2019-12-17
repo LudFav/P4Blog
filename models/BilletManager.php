081def38b49262512f598243ef5202263b64fa98
LudovicFavory <lu.favory@gmail.com>
2019-12-17 15:34:31 +0100

    Update BilletManager.php

--- models/BilletManager.php
+++ models/BilletManager.php
@@ -3,23 +3,34 @@
 
 class BilletManager extends Model implements crud
 {
+  public function create($table, $obj){
+    $this->getBdd();
+    $var = [];
+    $req = self::$_bdd->prepare('INSERT INTO billets SET auteur = :auteur, titre = :titre, contenu = :contenu, date = NOW()');   
+    $req->bindValue(':titre', $billet->titre());
+    $req->bindValue(':auteur', $billet->auteur());
+    $req->bindValue(':contenu', $billet->contenu());
+    $req->execute();
+    $req->closeCursor();
+  }
+
+
   public function readAll($table, $obj){
     $this->getBdd();
     $var = [];
-    $req = self::$_bdd->prepare('SELECT * FROM '.$table.' ORDER BY id desc');
+    $req = self::$_bdd->prepare('SELECT * FROM '.$table.' ORDER BY id DESC');
     $req->execute();
-
     //on crée la variable data qui
     //va contenir les données
     while ($data = $req->fetch(PDO::FETCH_ASSOC)) {
       // var contiendra les données sous forme d'objets
       $var[] = new $obj($data);
     }
-
     return $var;
     $req->closeCursor();
 
   }
+
 
   public function readOne($table, $obj, $id){
     $this->getBdd();
@@ -29,10 +40,16 @@ class BilletManager extends Model implements crud
     while ($data = $req->fetch(PDO::FETCH_ASSOC)) {
       $var[] = new $obj($data);
     }
-
     return $var;
     $req->closeCursor();
   }
+
+  public function update(){}
+
+  public function delete($id){
+    self::$_bdd->exec('DELETE FROM billets WHERE id = '.(int) $id);
+  }
+  
   //methode qui va recuperer tous les billets dans la bdd
   public function getBillets(){
     return $this->readAll('billets', 'Billet');
@@ -42,11 +59,9 @@ class BilletManager extends Model implements crud
       return $this->readOne('billets', 'Billet', $id);
   }
 
-  public function create(){}
+  
 
-  public function update(){}
-
-  public function delete(){}
+ 
   
 }
 
