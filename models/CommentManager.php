bba4a6c8e16b3faf6b49bfbe6c69f277df294d78
LudovicFavory <lu.favory@gmail.com>
2019-12-17 14:25:21 +0100

    Update CommentManager.php

--- models/CommentManager.php
+++ models/CommentManager.php
@@ -4,7 +4,36 @@ class CommentManager extends Model implements crud {
 
   //gérer la fonction qui va recuperer
   //tous les commentaires dans la bdd
-  
+  public function readAll($table, $obj){
+    $this->getBdd();
+    $var = [];
+    $req = self::$_bdd->prepare('SELECT * FROM '.$table.' ORDER BY id desc');
+    $req->execute();
+
+    //on crée la variable data qui
+    //va contenir les données
+    while ($data = $req->fetch(PDO::FETCH_ASSOC)) {
+      // var contiendra les données sous forme d'objets
+      $var[] = new $obj($data);
+    }
+
+    return $var;
+    $req->closeCursor();
+
+  }
+
+  public function readOne($table, $obj, $id){
+    $this->getBdd();
+    $var = [];
+    $req = self::$_bdd->prepare("SELECT id, auteur, titre, contenu, DATE_FORMAT(date, '%d/%m/%Y à %Hh%i') AS date FROM " .$table. " WHERE id = ?");
+    $req->execute(array($id));
+    while ($data = $req->fetch(PDO::FETCH_ASSOC)) {
+      $var[] = new $obj($data);
+    }
+
+    return $var;
+    $req->closeCursor();
+  }
 
 
   public function getComments($billetId){
