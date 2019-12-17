23fb4f7cc7a3509ee1a016ad2883bdec01b789c3
LudovicFavory <lu.favory@gmail.com>
2019-12-17 14:16:43 +0100

    Create ControllerAdmin.php

---
+++ controllers/ControllerAdmin.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'views/View.php';
+class ControllerAdmin{
+    private $_billetManager;
+    private $_commentManager;
+    private $_view;
+
+    private function tableauDeBord(){
+        $this->_view = new View('Admin');
+        if (isset($_GET['id'])) {
+          $this->_billetManager = new BilletManager;
+          $billets = $this->_billetManager->getBillets();
+     
+          $this->_commentManager = new CommentManager;
+          $commentaires = $this->_commentManager->getSignaledComments($_GET['id']);
+       
+          $this->_view->generate(array('billets' => $billets, 'commentaires' => $commentaires));
+        } 
+      }
+}
