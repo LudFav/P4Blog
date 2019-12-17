c922d36a3ddebfcc28ad05b345e112993b2aba57
LudovicFavory <lu.favory@gmail.com>
2019-12-17 14:25:19 +0100

    Update BilletManager.php

--- models/BilletManager.php
+++ models/BilletManager.php
@@ -3,14 +3,43 @@
 
 class BilletManager extends Model implements crud
 {
+  public function readAll($table, $obj){
+    $this->getBdd();
+    $var = [];
+    $req = self::$_bdd->prepare('SELECT * FROM '.$table.' ORDER BY id desc');
+    $req->execute();
 
+    //on crée la variable data qui
+    //va contenir les données
+    while ($data = $req->fetch(PDO::FETCH_ASSOC)) {
+      // var contiendra les données sous forme d'objets
+      $var[] = new $obj($data);
+    }
+
+    return $var;
+    $req->closeCursor();
+
+  }
+
+  public function readOne($table, $obj, $id){
+    $this->getBdd();
+    $var = [];
+    $req = self::$_bdd->prepare("SELECT id, auteur, titre, contenu, DATE_FORMAT(date, '%d/%m/%Y à %Hh%i') AS date FROM " .$table. " WHERE id = ?");
+    $req->execute(array($id));
+    while ($data = $req->fetch(PDO::FETCH_ASSOC)) {
+      $var[] = new $obj($data);
+    }
+
+    return $var;
+    $req->closeCursor();
+  }
   //methode qui va recuperer tous les billets dans la bdd
   public function getBillets(){
-    return $this->getAll('billets', 'Billet');
+    return $this->readAll('billets', 'Billet');
   }
 
   public function getBillet($id){
-      return $this->getOne('billets', 'Billet', $id);
+      return $this->readOne('billets', 'Billet', $id);
   }
 
   public function create(){}
