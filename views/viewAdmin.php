c2878e012bc4d2619c8ee086fef1ff617195ef65
LudovicFavory <lu.favory@gmail.com>
2019-12-19 15:10:36 +0100

    Update viewAdmin.php

--- views/viewAdmin.php
+++ views/viewAdmin.php
@@ -1,5 +1,5 @@
 <div class="slider">
   <div class="display-table  center-text">
-    <h1 class="title display-table-cell"><b>DESIGN</b></h1>
+    <h1 class="title display-table-cell"><b>ADMIN</b></h1>
   </div>
 </div><!-- slider -->
