0cf4556a93b4e64a304127f94fe04116a4801d59
LudovicFavory <lu.favory@gmail.com>
2019-12-19 15:31:26 +0100

    Update ControllerPost.php

--- controllers/ControllerPost.php
+++ controllers/ControllerPost.php
@@ -10,10 +10,7 @@ class ControllerPost {
     if (isset($url) && count($url) < 1) {
       throw new \Exception("Page Introuvable");
     } else {
-      $this->post();
-      //$this->billet();
-      //$this->commentaires();
-     
+      $this->post();    
     }
   }
 
@@ -29,29 +26,5 @@ class ControllerPost {
       $this->_view->generate(array('billet' => $billet, 'commentaires' => $commentaires));
     } 
   }
-/*
-  private function billet(){
-    if (isset($_GET['id'])) {
-      $this->_billetManager = new BilletManager;
-      $billet = $this->_billetManager->getBillet($_GET['id']);
-      $this->_view = new View('SinglePost');
-      $this->_view->generate(array('billet' => $billet), array('commentaires' => $commentaires) );
-    }
-  }
-
-  private function commentaires(){
-    var_dump('commentaires');
-    if (isset($_GET['id'])){
-      $this->_commentManager = new CommentManager;
-
-      $commentaires = $this->_commentManager->getComments($_GET['id']);
-      echo '<pre>';
-      var_dump($commentaires);
-      echo '</pre>';
-      $this->_view->generate(array('commentaires' => $commentaires));
-    }
-  }
-*/
-
 }
 
