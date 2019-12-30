0e52036d5d428aa67829af51aa2496f2f914f0bc
LudFav <45117462+LudFav@users.noreply.github.com>
2019-12-30 17:17:00 +0100

    Update viewAdmin.php

--- views/viewAdmin.php
+++ views/viewAdmin.php
@@ -3,3 +3,34 @@
     <h1 class="title display-table-cell"><b>ADMIN</b></h1>
   </div>
 </div><!-- slider -->
+
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+            <table>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Auteur</th>
+      <th>Titre</th>
+      <th>Date</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php foreach ($billets as $billet):
+     ?>
+    <tr>
+      <td><?php echo ($billet->id()); ?></td>
+      <td><?php echo ($billet->auteur()); ?></td>
+      <td><?php echo ($billet->titre()); ?></td>
+      <td><?php echo ($billet->date()); ?></td>
+  </tr>
+  <?php endforeach; ?>
+  </tbody>
+</table>
+                    
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
