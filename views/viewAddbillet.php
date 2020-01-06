25828a8d3e18fbb5cf75fcb009ef7fba76fd6e63
LudovicFavory <lu.favory@gmail.com>
2020-01-06 14:58:46 +0100

    Create viewAddbillet.php

---
+++ views/viewAddbillet.php
@@ -0,0 +1,11 @@
+<div>
+    <form>
+        <label> Auteur</label>
+        <input>
+        <label> Titre</label>
+        <input>
+        <label> Contenu</label>
+        <textarea>
+    </form>
+</div>
+
