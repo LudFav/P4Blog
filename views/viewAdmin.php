a37f65e544e81f81917b7d7d1634b5abb78f6be8
LudovicFavory <lu.favory@gmail.com>
2020-01-06 11:30:22 +0100

    Update viewAdmin.php

--- views/viewAdmin.php
+++ views/viewAdmin.php
@@ -42,9 +42,47 @@
                     <?php endforeach; ?>
                   </tbody>   
             </table> 
+            </div>     
+            <div class="table-wrapper">
+            <div class="table-title">
+                <div class="row">
+                    <div class="col-sm-6">
+						          <h2>Commentaires Signalés</h2>
+                    </div>
+					          <div class="col-sm-6">
+						          <a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i> <span>Effacer</span></a>						
+					          </div>
+                </div>
+            </div>
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Auteur</th>
+						            <th>Contenu</th>
+                        <th>Date</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                 <?php /* foreach ($commentaires as $commentaire): ?>
+                      <tr>
+                        <td><?php echo ($commentaire->id()); ?></td>
+                        <td><?php echo ($commentaire->auteur()); ?></td>
+                        <td><?php echo ($commentaire->contenu()); ?></td>
+                        <td><?php echo ($$commentaire->date()); ?></td>
+                        <td>
+                          <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+                          <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+                        </td>
+                      </tr>
+                    <?php endforeach; */ ?>
+                
+                  </tbody>   
+            </table> 
             </div>        
-        </div>
-    </div>
+        </div>   
+      </div>  
 </body>
 </html>
 
