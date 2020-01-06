1723ec992dcb968bc6d3d63895aee45b4b18a69d
LudovicFavory <lu.favory@gmail.com>
2020-01-06 11:37:01 +0100

    Update Model.php

--- models/Model.php
+++ models/Model.php
@@ -21,39 +21,5 @@ abstract class Model
       return self::$_bdd;
     }
   }
-
-  //creation de la methode de récupération de liste d'elements dans la bdd
-/*
-  protected function getAll($table, $obj){
-    $this->getBdd();
-    $var = [];
-    $req = self::$_bdd->prepare('SELECT * FROM '.$table.' ORDER BY id desc');
-    $req->execute();
-
-    //on crée la variable data qui
-    //va contenir les données
-    while ($data = $req->fetch(PDO::FETCH_ASSOC)) {
-      // var contiendra les données sous forme d'objets
-      $var[] = new $obj($data);
-    }
-
-    return $var;
-    $req->closeCursor();
-
-  }
-
-  protected function getOne($table, $obj, $id){
-    $this->getBdd();
-    $var = [];
-    $req = self::$_bdd->prepare("SELECT id, auteur, titre, contenu, DATE_FORMAT(date, '%d/%m/%Y à %Hh%i') AS date FROM " .$table. " WHERE id = ?");
-    $req->execute(array($id));
-    while ($data = $req->fetch(PDO::FETCH_ASSOC)) {
-      $var[] = new $obj($data);
-    }
-
-    return $var;
-    $req->closeCursor();
-  }
-  */
 }
 
