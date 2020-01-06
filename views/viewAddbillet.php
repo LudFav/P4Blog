1acbf26cdff7a5119b814000ca4b5b984a280ba2
LudovicFavory <lu.favory@gmail.com>
2020-01-06 15:51:03 +0100

    Update viewAddbillet.php

--- views/viewAddbillet.php
+++ views/viewAddbillet.php
@@ -1,11 +1,18 @@
-<div>
-    <form>
-        <label> Auteur</label>
-        <input>
-        <label> Titre</label>
-        <input>
-        <label> Contenu</label>
-        <textarea>
-    </form>
-</div>
+<form>
+  <div class="form-group">
+    <label for="auteur">Auteur</label>
+    <input type="text" class="form-control" id="Test" placeholder="Stephane Rois">
+  </div>
 
+  <div class="form-group">
+    <label for="titre">Titre</label>
+    <input type="text" class="form-control" id="titre" placeholder="titre">
+  </div>
+
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Example textarea</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+</form>
+</body>
+</html>
