d239651d56969aed381f843cad06da942b265b72
LudovicFavory <lu.favory@gmail.com>
2020-01-07 11:20:32 +0100

    Update viewAddbillet.php

--- views/viewAddbillet.php
+++ views/viewAddbillet.php
@@ -1,17 +1,17 @@
 
 <form action="" method="post">
   <div class="form-group">
-    <label for="auteur">Auteur</label>
-    <input type="text" class="form-control" id="Test" placeholder="Stephane Rois">
+    <label for="auteur"><b>Auteur</b></label>
+    <input type="text" class="form-control" id="Test" placeholder="Stephane Rois" size="35">
   </div>
 
   <div class="form-group">
-    <label for="titre">Titre</label>
+    <label for="titre"><b>Titre</b></label>
     <input type="text" class="form-control" id="titre" placeholder="titre">
   </div>
 
   <div class="form-group">
-    <label for="exampleFormControlTextarea1">Example textarea</label>
+    <label for="exampleFormControlTextarea1"><b>Contenu</b></label>
     <textarea class="form-control" id="textarea1" rows="3"></textarea>
   </div>
 
