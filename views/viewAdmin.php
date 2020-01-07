e9bd47e486f645e160bb26d1c509f39c116763ca
LudovicFavory <lu.favory@gmail.com>
2020-01-07 14:43:34 +0100

    Update viewAdmin.php

--- views/viewAdmin.php
+++ views/viewAdmin.php
@@ -12,7 +12,7 @@
 						          <h2>Billets</h2>
                     </div>
 					          <div class="col-sm-6">
-						          <a href=".../addbillet" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Ajouter 1 billet</span></a>
+						          <a href="addbillet" class="btn btn-success"><i class="material-icons">&#xE147;</i> <span>Ajouter 1 billet</span></a>
 						          <a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i> <span>Effacer</span></a>						
 					          </div>
                 </div>
