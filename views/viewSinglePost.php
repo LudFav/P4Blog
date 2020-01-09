9ff20c02469dfc5cf30f503a942391e98204a254
LudovicFavory <lu.favory@gmail.com>
2020-01-09 16:16:40 +0100

    Update viewSinglePost.php

--- views/viewSinglePost.php
+++ views/viewSinglePost.php
@@ -63,8 +63,10 @@
 
 <section class="comment-section">
   <div class="container">
+  <?php
   
-    <h4><b>Commentaires</b></h4>
+  ?>
+    <!--<h4><b>Commentaires</b></h4>
     <div class="row">
 
       <div class="col-lg-8 col-md-12">
@@ -75,18 +77,18 @@
               <div class="col-sm-6">
                 <input type="text" aria-required="true" name="auteur" class="form-control"
                   placeholder="Nom" aria-invalid="true" required >
-              </div><!-- col-sm-6 -->
+              </div>
               <div class="col-sm-12">
                 <textarea name="contenu" rows="2" class="text-area-messge form-control"
                   placeholder="Enter your comment" aria-required="true" aria-invalid="false"></textarea >
-              </div><!-- col-sm-12 -->
+              </div>
               <div class="col-sm-12">
                 <button class="submit-btn" type="submit" id="form-submit"><b>Envoyer Commentaire</b></button>
-              </div><!-- col-sm-12 -->
+              </div>
 
-            </div><!-- row -->
+            </div>
           </form>
-        </div><!-- comment-form -->
+        </div>-->
         <?php
         foreach ($commentaires as $commentaire):  
         ?>
