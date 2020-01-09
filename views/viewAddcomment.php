7504ab7d745f61a22cfd8569845d4aa67f4963f1
LudovicFavory <lu.favory@gmail.com>
2020-01-09 16:16:46 +0100

    Create viewAddcomment.php

---
+++ views/viewAddcomment.php
@@ -0,0 +1,23 @@
+<h4><b>Commentaires</b></h4>
+    <div class="row">
+
+      <div class="col-lg-8 col-md-12">
+        <div class="comment-form">
+          <form action="" method="post">
+            <div class="row">
+
+              <div class="col-sm-6">
+                <input type="text" aria-required="true" name="auteur" class="form-control"
+                  placeholder="Nom" aria-invalid="true" required >
+              </div><!-- col-sm-6 -->
+              <div class="col-sm-12">
+                <textarea name="contenu" rows="2" class="text-area-messge form-control"
+                  placeholder="Enter your comment" aria-required="true" aria-invalid="false"></textarea >
+              </div><!-- col-sm-12 -->
+              <div class="col-sm-12">
+                <button class="submit-btn" type="submit" id="form-submit"><b>Envoyer Commentaire</b></button>
+              </div><!-- col-sm-12 -->
+
+            </div><!-- row -->
+          </form>
+        </div><!-- comment-form -->
