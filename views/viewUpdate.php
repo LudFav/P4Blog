0db650565336647b877eb96d228b873fd8125a31
LudovicFavory <lu.favory@gmail.com>
2020-01-13 17:38:32 +0100

    Update viewUpdate.php

--- views/viewUpdate.php
+++ views/viewUpdate.php
@@ -1,19 +1,15 @@
 
-<form action="update" method="post" id="FormAddBillet">
-  <div class="form-group">
-    <label for="auteur" class="labelBillet"><b>Auteur</b></label>
-    <input type="text" name="auteur" class="form-control" id="auteurB" size="35" value="<?=$billet[0]->auteur()?>">
-  </div>
+<form action="" method="post" id="FormAddBillet">
 
   <div class="form-group">
     <label for="titre" class="labelBillet"><b>Titre</b></label>
-    <input type="text" name="titre" class="form-control" id="titreB" value="<?=$billet->titre()?>">
+    <input type="text" name="titre" class="form-control" id="titreB" value="<?=$billet[0]->titre()?>">
   </div>
 
   <div class="form-group">
     <label for="contenu" class="labelBillet"><b>Contenu</b></label>
-    <textarea class="form-control" name="contenu" id="textareaB" rows="3" value="<?=$billet->contenu()?>"></textarea>
+    <textarea class="form-control" name="contenu" id="textareaC" rows="3"><?=$billet[0]->contenu()?></textarea>
   </div>
 
-  <input name="updateBillet" type="submit" value="valider" class="updateBillet"/>
+  <input name="updateBillet" type="submit" value="valider" class="addBillet"/>
 </form>
