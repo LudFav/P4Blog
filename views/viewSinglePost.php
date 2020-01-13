1e23ebd27bc3258785008f0583a30643dd72f1fc
LudovicFavory <lu.favory@gmail.com>
2020-01-13 10:27:58 +0100

    Update viewSinglePost.php

--- views/viewSinglePost.php
+++ views/viewSinglePost.php
@@ -66,7 +66,7 @@
   <?php
   
   ?>
-    <!--<h4><b>Commentaires</b></h4>
+    <h4><b>Commentaires</b></h4>
     <div class="row">
 
       <div class="col-lg-8 col-md-12">
@@ -83,12 +83,12 @@
                   placeholder="Enter your comment" aria-required="true" aria-invalid="false"></textarea >
               </div>
               <div class="col-sm-12">
-                <button class="submit-btn" type="submit" id="form-submit"><b>Envoyer Commentaire</b></button>
+                <button class="submit-btn" type="submit" id="form-submit" name="addComment"><b>Envoyer Commentaire</b></button>
               </div>
 
             </div>
           </form>
-        </div>-->
+        </div>
         <?php
         foreach ($commentaires as $commentaire):  
         ?>
