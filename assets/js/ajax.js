b796a6e7aa68822d3c1259b38e0be7eb98aa5e3d
LudovicFavory <lu.favory@gmail.com>
2020-01-14 16:57:21 +0100

    Update ajax.js

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,28 +1,10 @@
-<button type="button" class="deleteProduct" data-token="{{ csrf_token() }}">Confirm</button>
-
-$('.deleteProduct').on('click', function(e) {
-    var inputData = $('#formDeleteProduct').serialize()+"&_method=delete&_token="+$(this).data(token);
-
-    var dataId = $('#btnDeleteProduct').attr('data-id');
-
-    $.ajax({
-        url: '{{ url('/admin/products') }}' + '/' + dataId,
-        type: 'POST',
-        data: inputData,
-        success: function( msg ) {
-            if ( msg.status === 'success' ) {
-                toastr.success( msg.msg );
-                setInterval(function() {
-                    window.location.reload();
-                }, 5900);
-            }
-        },
-        error: function( data ) {
-            if ( data.status === 422 ) {
-                toastr.error('Cannot delete the category');
-            }
-        }
+function signalement(){
+    $('#signal<?= echo $commentaire->id()?>').on('click', function(e) {       
+        $.post({
+             url: 'ControllerPost.php',
+             data: {
+             'signal': 'true' 
+            }    
+        });
     });
-
-    return false;
-});
+}
