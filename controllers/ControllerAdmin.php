ce0b0e0de29c6691ec64787071af7dfee44321cd
LudovicFavory <lu.favory@gmail.com>
2020-01-16 12:55:00 +0100

    Update ControllerAdmin.php

--- controllers/ControllerAdmin.php
+++ controllers/ControllerAdmin.php
@@ -13,13 +13,33 @@ class ControllerAdmin{
 
     private function tableauDeBord(){
        
-        $this->_view = new View('Admin');
+        
         $this->_billetManager = new BilletManager;
         $billets = $this->_billetManager->getBillets();
-        
+        if (isset($_GET['id'])) {
+          $this->_billetManager = new BilletManager;
+          $deleteBillet = $this->_billetManager->deleteBillet($_GET['id']);
+          header('Location:admin');
+        }
+
         $this->_commentManager = new CommentManager;
         $commentaires = $this->_commentManager->getSignaledComments('commentaires', 'Comment', $signale=null);
-     
+        if(isset($_POST['unSignal'])){
+          $unSignalComment = $this->_commentManager->unsignaleComment($_POST['unSignal']);
+          header('Location:admin');
+        }
+
+        if(isset($_POST['modere'])){
+          $modereComment = $this->_commentManager->modereComment($_POST['modere']);
+          header('Location:admin');
+        }
+
+        if (isset($_POST['delete'])) {
+          $deleteComment = $this->_commentManager->deleteComment($_POST['delete']);
+          header('Location:admin');
+        }
+
+      $this->_view = new View('Admin');
       $this->_view->generate(array('billets' => $billets, 'commentaires' => $commentaires ));  
     }
 }
