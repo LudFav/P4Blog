064e708831a07fdaaabfdf7e4148e8e5189a045e
LudovicFavory <lu.favory@gmail.com>
2020-01-16 12:54:58 +0100

    Update ajax.js

--- controllers/ajax.js
+++ controllers/ajax.js
@@ -15,6 +15,45 @@
                 
                 });
         });
+
+
+// ADMIN
+        $('.unsignalComBtn').on('click',function(){
+            var idComToUnsignal=$(this).attr('value');
+            console.log(idComToUnsignal);
+            $.post({
+                 url: 'admin',
+                 data: {'unSignal' : idComToUnsignal},
+                 /*success: function(data){
+                    console.log(data);
+                }*/
+            });
+        });
+
+        $('.modereComBtn').on('click',function(){
+            var idComToModere=$(this).attr('value');
+            console.log(idComToModere);
+            $.post({
+                 url: 'admin',
+                 data: {'modere' : idComToModere},
+                 /*success: function(data){
+                    console.log(data);
+                }*/
+            });
+        });
+
+        $('.deleteComBtn').on('click',function(){
+            var idComToDelete=$(this).attr('value');
+            console.log(idComToDelete);
+            $.post({
+                 url: 'admin',
+                 data: {'delete' : idComToDelete},
+                 /*success: function(data){
+                    console.log(data);
+                }*/
+            });
+        });
+   
             
 
     
