42af329b28f6e238b8a016645237da7e59478e1c
LudovicFavory <lu.favory@gmail.com>
2020-01-21 18:00:19 +0100

    Update viewAdmin.php

--- views/viewAdmin.php
+++ views/viewAdmin.php
@@ -16,7 +16,7 @@
 					          </div>
                 </div>
             </div>
-            <table class="table table-striped table-hover">
+            <table class="table table-striped table-hover" id='tableBillet'>
                 <thead>
                     <tr>
                         <th>#</th>
@@ -26,23 +26,10 @@
                         <th>Actions</th>
                     </tr>
                 </thead>
-                <tbody>
-                    <?php foreach ($billets as $billet): ?>
-
-                      <tr>
-                        <a href="post&id=<?= $billet->id()?>"><td><?php echo ($billet->id()); ?></td></a>
-                        <td><?php echo ($billet->auteur()); ?></td>
-                        <td><?php echo ($billet->titre()); ?></td>
-                        <td><?php echo ($billet->date()); ?></td>
-                        <td class="actionTd">
-                          <button class="visualBilBtn"><a href="post&id=<?= $billet->id()?>" ><i class="fa fa-eye"></i></a></button>
-                          <button class="editBilBtn" ><a href="update&id=<?= $billet->id()?>"><i class="fa fa-pencil" aria-hidden="true"></i></a></button>
-                          <button class="deleteBilBtn" value="<?= $billet->id()?>" ><i class="fa fa-trash" aria-hidden="true"></i></button>
-                        </td>
-                      </tr>
-                    <?php endforeach; ?>
-                  </tbody>   
-            </table> 
+                <tbody id="tbodyBillet">
+                    
+                </tbody>   
+            </table>
             </div>     
             <div class="table-wrapper">
             <div class="table-title">
@@ -52,7 +39,7 @@
                     </div>
                 </div>
             </div>
-            <table class="table table-striped table-hover">
+            <table class="table table-striped table-hover" id='tableComments'>
                 <thead>
                     <tr>
                         <th>#</th>
@@ -62,24 +49,10 @@
                         <th>Actions</th>
                     </tr>
                 </thead>
-                <tbody>
-                 <?php  foreach ($commentaires as $commentaire): ?>
-                      <tr>
-                        <td><?php echo ($commentaire->id()); ?></td>
-                        <td><?php echo ($commentaire->auteur()); ?></td>
-                        <td><?php echo ($commentaire->contenu()); ?></td>
-                        <td><?php echo ($commentaire->date()); ?></td>
-                        <td class="actionTd">
-                          
-                          <button value="<?= $commentaire->id()?>" class="unsignalComBtn" ><i class="fa fa-comment-o" aria-hidden="true"></i></button>
-                          <button value="<?= $commentaire->id()?>" class="modereComBtn" ><i class="fa fa-commenting" aria-hidden="true"></i></button>
-                          <button value="<?= $commentaire->id()?>" class="deleteComBtn" ><i class="fa fa-trash" aria-hidden="true"></i></button>
-                          <!--<form action="deletecomment&id=<?= $commentaire->id()?>" id="commentDelete<?= $commentaire->id()?>" onSubmit="return confirm('Etes vous sur de vouloir supprimer ce commentaire?')" class="deleteForm"><button name="deleteCommentbtn" type="submit" id="deleteButton<?= $commentaire->id()?>" class="deletebtn"><i class="fa fa-trash" aria-hidden="true"></i></button></form>-->
-                        </td>
-                      </tr>
-                    <?php endforeach;  ?>
+                <tbody id='tbodyComment'>
+       
                 
-                  </tbody>   
+                </tbody>   
             </table> 
             </div>        
         </div>   
