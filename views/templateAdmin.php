69d99a83651a29174b014a0ab2cc3d3441d1b394
LudovicFavory <lu.favory@gmail.com>
2020-01-21 18:00:37 +0100

    Create templateAdmin.php

---
+++ views/templateAdmin.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>TITLE</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8">
+	
+
+	<!-- Font -->
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
+	<link href="assets/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+
+	<!-- Stylesheets -->
+
+	<link href="public/common-css/bootstrap.css" rel="stylesheet">
+
+	<link href="public/common-css/ionicons.css" rel="stylesheet">
+
+
+	<link href="public/layout-1/css/styles.css" rel="stylesheet">
+
+	<link href="public/layout-1/css/responsive.css" rel="stylesheet">
+
+</head>
+<body >
+
+	<header>
+		<div class="container-fluid position-relative no-side-padding">
+
+			<a href="#" class="logo"><img src="public/images/logo.png" alt="Logo Image"></a>
+
+			<div class="menu-nav-icon" data-nav-menu="#main-menu"><i class="ion-navicon"></i></div>
+
+			<ul class="main-menu visible-on-click" id="main-menu">
+				<li><a href="accueil">Accueil</a></li>
+				<li><a href="#">Categories</a></li>
+				<li><a href="login">Connexion</a></li>
+			</ul><!-- main-menu -->
+
+			<div class="src-area">
+				<form>
+					<button class="src-btn" type="submit"><i class="ion-ios-search-strong"></i></button>
+					<input class="src-input" type="text" placeholder="Type of search">
+				</form>
+			</div>
+
+		</div><!-- conatiner -->
+	</header>
+
+
+  <?= $content ?>
+
+	<footer>
+
+		<div class="container">
+			<div class="row">
+
+				<div class="col-lg-4 col-md-6">
+					<div class="footer-section">
+
+						<a class="logo" href="#"><img src="public/images/logo.png" alt="Logo Image"></a>
+						<p class="copyright">Bona @ 2017. All rights reserved.</p>
+						
+
+					</div><!-- footer-section -->
+				</div><!-- col-lg-4 col-md-6 -->
+
+				
+						<h4 class="title"><b>SUBSCRIBE</b></h4>
+						<div class="input-area">
+							<form>
+								<input class="email-input" type="text" placeholder="Enter your email">
+								<button class="submit-btn" type="submit"><i class="icon ion-ios-email-outline"></i></button>
+							</form>
+						</div>
+
+					</div><!-- footer-section -->
+				</div><!-- col-lg-4 col-md-6 -->
+
+			</div><!-- row -->
+		</div><!-- container -->
+	</footer>
+
+
+	<!-- SCIPTS -->
+	
+	<script src="public/common-js/jquery-3.1.1.min.js"></script>
+	<script src="public/common-js/tether.min.js"></script>
+	<script src="public/common-js/bootstrap.js"></script>
+	<script src="public/common-js/scripts.js"></script>
+
+	<script src="assets/js/tinymce/tinymce.min.js"></script>
+	<script> tinymce.init({
+	  selector: '#textareaB, #textareaC',  // change this value according to your HTML
+	  plugin: 'a_tinymce_plugin',
+	  a_plugin_option: true,
+	  a_configuration_option: 400
+	});
+	</script>
+	<script src="controllers/ajaxAdmin.js"></script>
+</body>
+</html>
