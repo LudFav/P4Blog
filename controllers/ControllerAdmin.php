02a798e6655d498232432d81c5104aeb50b28d33
LudovicFavory <lu.favory@gmail.com>
2020-01-22 15:47:06 +0100

    Update ControllerAdmin.php

--- controllers/ControllerAdmin.php
+++ controllers/ControllerAdmin.php
@@ -3,43 +3,12 @@
 require_once 'views/View.php';
 
 class ControllerAdmin{
-    private $_billetManager;
-    private $_commentManager;
+    
     private $_view;
     public function __construct(){
-      $this->tableauDeBord();
-    }
-  
 
-    private function tableauDeBord(){
-       
-        //BILLET
-        $this->_billetManager = new BilletManager;
-        $billets = ' ';//$this->_billetManager->getBillets();
-        /*if (isset($_POST['deleteBillet'])){
-          $deleteBillet = $this->_billetManager->deleteBillet($_POST['deleteBillet']);
-        }*/
-
-
-        //COMMENT
-       /*  $this->_commentManager = new CommentManager;
-        $commentaires = $this->_commentManager->getSignaledComments('commentaires', 'Comment', $signale=null);
-        
-
-       if(isset($_POST['unSignal'])){
-          $unSignalComment = $this->_commentManager->unsignaleComment($_POST['unSignal']);  
-        }
-
-        if(isset($_POST['modere'])){
-          $modereComment = $this->_commentManager->modereComment($_POST['modere']);  
-        }
-
-        if (isset($_POST['deleteCom'])) {
-          $deleteComment = $this->_commentManager->deleteComment($_POST['deleteCom']);
-          
-        }*/
       //VUE
       $this->_view = new View('Admin'); 
-      $this->_view->generateAdmin(array(/*'billets' => $billets, 'commentaires' => $commentaires */));  
+      $this->_view->generateAdmin(array());  
     }
 }
