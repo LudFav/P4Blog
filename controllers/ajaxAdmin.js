30c4cef4c48318e1ab80a52a292b14761a6d43e3
LudovicFavory <lu.favory@gmail.com>
2020-01-22 12:04:22 +0100

    Update ajaxAdmin.js

--- controllers/ajaxAdmin.js
+++ controllers/ajaxAdmin.js
@@ -1,6 +1,5 @@
 // ADMIN
-billetTable();
-commentTable();
+
     //TABLE BILLETS
     function billetTable(){
         var idBillet = $('.post-info').attr('value');
@@ -14,26 +13,6 @@ commentTable();
         })
     }    
     
-    
-    
-   // $(document).ready(adminActions);
-
-    $(document).ready(function(){
-        //BOUTON BILLETS
-        $('.deleteBilBtn').on('click', function() {
-            var idBilToDelete=$(this).attr('value');
-            console.log('test01');
-           /* $.post({
-                 url: 'adminajax',
-                 data: {'action': 'deleteBillet','deleteBillet' : idBilToDelete},
-                 success: function(data){
-                    billetTable();
-                }
-            });*/
-        });
-        
-    })
-
 
     //TABLE COMMENTAIRES SIGNALÉS
     function commentTable(){
@@ -47,46 +26,68 @@ commentTable();
             }
         })
     }
-    console.log('test03');  
+     
+    billetTable();
+    commentTable();
+    console.log('test03'); 
     
-    
-    $(document).ready(function(){
-        console.log('test04');  
+//ACTIONS D'ADMINISTRATION
+
+    $(window).bind('load', function(){
+        console.log('test04'); 
+        //BOUTON BILLETS
+        $('.deleteBilBtn').on('click', function() {
+            var idBilToDelete=$(this).attr('value');
+            console.log('test07');
+            $.post({
+                url: 'adminajax',
+                data: {'action': 'deleteBillet','deleteBillet' : idBilToDelete},
+                success: function(data){
+                   billetTable();
+                }
+            });
+        }); 
 
         //BOUTONS COMMENTAIRES SIGNALÉS
         $('.unsignalComBtn').on('click', function() {
-            console.log('test 03');
-            //var idComToUnsignal=$(this).attr('value');
-           /* $.post({
-                 url: 'adminajax',
-                 data: {'action': 'unsignal', 'comToUnsignal' : idComToUnsignal},
-                 success: function(data){
-                    console.log(data);
+            console.log('test 08');
+            var idComToUnsignal=$(this).attr('value');
+            $.post({
+                url: 'adminajax',
+                data: {'action': 'unsignal', 'comToUnsignal' : idComToUnsignal},
+                success: function(data){
+                   console.log(data);
+                   commentTable();
                 }
-            });*/
+            });
         });
 
         $('.modereComBtn').on('click', function(){
             var idComToModere=$(this).attr('value');
+            console.log('test 08');
             console.log(idComToModere);
-            /*$.post({
-                 url: 'admin',
-                 data: {'action': 'moderer', 'modere' : idComToModere},
-                 success: function(data){
-                    console.log(data);
+            $.post({
+                url: 'adminajax',
+                data: {'action': 'moderer', 'modere' : idComToModere},
+                success: function(data){
+                   console.log(data);
+                   commentTable();
                 }
-            });*/
+            });
         });
 
         $('.deleteComBtn').on('click', function(){
             var idComToDelete=$(this).attr('value');
+            console.log('test 08');
             console.log(idComToDelete);
-            /*$.post({
-                 url: 'admin',
-                 data: {'action': 'deleteComment','deleteCom' : idComToDelete},
-                 success: function(data){
-                    console.log(data);  
-            });*/            
+            $.post({
+                url: 'adminajax',
+                data: {'action': 'deleteComment','deleteCom' : idComToDelete},
+                success: function(data){
+                   console.log(data);
+                   commentTable();
+                } 
+            });            
         });
         console.log('test05');  
 
