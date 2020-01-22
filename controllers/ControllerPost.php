a098cc6c73da52f254072fd78599dc989f998d92
LudovicFavory <lu.favory@gmail.com>
2020-01-22 15:47:11 +0100

    Update ControllerPost.php

--- controllers/ControllerPost.php
+++ controllers/ControllerPost.php
@@ -3,7 +3,6 @@ require_once 'views/View.php';
 
 class ControllerPost {
   private $_billetManager;
-  private $_commentManager;
   private $_view;
 
   public function __construct(){
@@ -25,42 +24,13 @@ class ControllerPost {
       $billet = $this->_billetManager->getBillet($_GET['id']);
       
 
-  //COMMENTAIRES  
-   
-   /*      $this->_commentManager = new CommentManager;
- 
-        /**
-     * Commentaires a montrer
-  /
-      $commentaires = $this->_commentManager->getComments($_GET['id']);
-
-        /**
-     * Inserer commentaires 
-  /
-      if(isset($_POST['addComment'])){
-        $data = array(
-            'billetId' => $_GET['id'],
-            'auteur' => htmlspecialchars($_POST['auteur']),
-            'contenu' => htmlspecialchars($_POST['contenu']),
-        );
-        $addComment = $this->_commentManager->createComment($data, $data['billetId']);
-        header('Location:post&id=' .$_GET['id']. '');
-        exit;
-      }
-        /**
-     * Commentaires a montrer
-  /
-        if(isset($_POST['idSignal'])){
-        $signaleCom = $this->_commentManager->signaleComment($_POST['idSignal']);   
-      }
-*/
 
     //VUE
         /**
      * Generation de la vue 
      */
     $this->_view = new View('SinglePost');
-$this->_view->generate(array('billet' => $billet/*, 'commentaires' => $commentaires*/)); 
+    $this->_view->generate(array('billet' => $billet)); 
     }
   }
 }
