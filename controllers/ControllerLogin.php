c70babc0a186450c9146e5ea67f023945c372c6d
LudovicFavory <lu.favory@gmail.com>
2020-01-22 15:37:37 +0100

    Update ControllerLogin.php

--- controllers/ControllerLogin.php
+++ controllers/ControllerLogin.php
@@ -1,23 +1,20 @@
 <?php
-require_once 'views/View.php';
+
 
 class ControllerLogin {
-    private $_UserManager;
+    private $_userManager;
     private $_view;
   
     public function __construct(){
-      if (isset($url) && count($url) < 1) {
-        throw new \Exception("Page Introuvable");
-      } else {
-        $this->auth();    
-      }
-    }
   
-    private function auth(){
-
-      $this->_view = new View('login');
-    }  
+    $this->_userManager = new UserManager;
+    if(isset($_POST['action']) && $_POST['action']=='login'){
+    var_dump($_POST['username']);
+    var_dump($_POST['password']);
+    }
+    
+    
   }
   
-  
+} 
 ?>
