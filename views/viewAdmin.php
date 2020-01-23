43f8a819f25865b20f55d4e531597099b70f442e
LudovicFavory <lu.favory@gmail.com>
2020-01-23 17:33:15 +0100

    Update viewAdmin.php

--- views/viewAdmin.php
+++ views/viewAdmin.php
@@ -3,7 +3,7 @@
     <h1 class="title display-table-cell"><b>ADMINISTRATION</b></h1>
   </div>
 </div><!-- slider -->
-
+<!--BILLET--> 
 <div class="container" style="padding-top:20px;">
         <div class="table-wrapper">
             <div class="table-title">
@@ -28,9 +28,29 @@
                 </thead>
                 <tbody id="tbodyBillet">
                     
-                </tbody>   
+                </tbody>  
             </table>
             </div>     
+<!--MODAL--> 
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="false">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal" id="closebtn">Close</button>       
+      </div>
+    </div>
+  </div>
+</div>
+<!--COMMENTAIRE SIGNALÉ-->
             <div class="table-wrapper">
             <div class="table-title">
                 <div class="row">
@@ -44,7 +64,7 @@
                     <tr>
                         <th>#</th>
                         <th>Auteur</th>
-						            <th>Contenu</th>
+						<th>Contenu</th>
                         <th>Date</th>
                         <th>Actions</th>
                     </tr>
