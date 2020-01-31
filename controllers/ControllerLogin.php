f5e9793e1db69ad9d36df793bcb806108284ef1b
LudFav <45117462+LudFav@users.noreply.github.com>
2020-01-31 13:09:14 +0100

    Update ControllerLogin.php

--- controllers/ControllerLogin.php
+++ controllers/ControllerLogin.php
@@ -8,13 +8,32 @@ class ControllerLogin {
     public function __construct(){
   
     $this->_userManager = new UserManager;
-    if(isset($_POST['action']) && $_POST['action']=='login'){
-    var_dump($_POST['username']);
-    var_dump($_POST['password']);
-    }
-    
-    
+    $this->login(); 
   }
-  
+    
+  public function login() {
+    $userInfo = $this->_userManager->getUser();
+    //$passToHash = password_hash('admin', PASSWORD_DEFAULT, ['cost' => 12]);
+    if(isset($_POST['action']) && $_POST['action']=='login'){
+      if($_POST['username'] == $userInfo[0]->username()){
+        echo "Votre pseudo est OK";
+      } else {
+        echo "Erreur : Mauvais pseudonyme </br>";
+      }
+    $passwordSubmitted = $_POST['password'];
+    $passwordHashed = $userInfo[0]->password();
+    $password= password_verify($passwordSubmitted, $passwordHashed);
+    
+      if($password == true){
+        echo "Votre mot de passe est OK";
+      } else {
+        echo "Erreur : Mauvais mot de passe";
+      }
+    }
+  }
+
+  public function logout() {
+
+  }
 } 
 ?>
