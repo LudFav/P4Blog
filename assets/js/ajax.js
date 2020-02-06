f8d3b91436a2c61160b524fa43dc985d156e22d5
LudovicFavory <lu.favory@gmail.com>
2020-02-06 14:57:21 +0100

    Update ajax.js

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,103 +1,115 @@
 
 //  AJAX FRONT
-
-        function showComment(){
-            var idBillet = $('.post-info').attr('value');
-            $.post({
-                url: 'ajax',
-                data:{'billetId': idBillet, 'action': 'showComment'},
-                success: function(data){
-                    if(!$.trim(data)){
-                        $('#showComments').text('Soyez la première personne à écrire un commentaire sur ce billet.'); 
-                        $('#showComments').attr('style','font-style:italic; margin-bottom:15px;');       
-                    } else{
-                        let newCommentDisplay= $(data);
-                        newButtonSignal = newCommentDisplay.find('.signalbtn');
-                        newButtonSignal.on('click', function(){
-                            id= $(this).attr('value');
-                            signalement(id);
-                        });
-
-                        $('#showComments').html(newCommentDisplay);
-                    }
-                }
-            })
+function billetAccueil(){ 
+    let url_string = window.location.href;
+    let url = new URL(url_string);
+    page = url.searchParams.get("page");
+   $.post({
+       url: 'ajax',
+       data:{'action': 'showAccueilBillet', 'page': page},
+       success: function(data){
+           let billetsAccueil = $(data);
+           $('#billetAccueil').html(billetsAccueil);
+           }
+       }) 
+} 
+function pagination(){
+    $.post({
+        url: 'ajax',
+        data:{'action': 'paginationAccueil', 'page': page},
+        success: function(data){
+           let pagination = $(data);
+           $('#paginationAccueil').html(pagination);
         }
-
-        //FORMULAIRE D'ENVOI DE COMMENTAIRE
-        $('.submit-btn').on('click', function(e){
-            e.preventDefault();
-            if($('#formCommentaire')[0].checkValidity()){
-                var idBillet = $('.post-info').attr('value');
-                var auteur = $('#auteur').val();
-                var contenu = $('#contenu').val();
-                $.post({
-                    url:'ajax',
-                    data:{'action': 'insertCom',
-                          'auteur': auteur,
-                          'contenu': contenu,
-                          'billetId': idBillet},
-                    success:function(data){
-                        $('#formCommentaire')[0].reset(); 
-                        showComment();
-                    }
-                })   
+    })
+}
+        
+function showComment(){
+        var idBillet = $('.post-info').attr('value');
+        $.post({
+            url: 'ajax',
+            data:{'billetId': idBillet, 'action': 'showComment'},
+            success: function(data){
+                if(!$.trim(data)){
+                    $('#showComments').text('Soyez la première personne à écrire un commentaire sur ce billet.'); 
+                    $('#showComments').attr('style','font-style:italic; margin-bottom:15px;');       
+                } else{
+                    let newCommentDisplay= $(data);
+                    newButtonSignal = newCommentDisplay.find('.signalbtn');
+                    newButtonSignal.on('click', function(){
+                        id= $(this).attr('value');
+                        signalement(id);
+                    });
+                    $('#showComments').html(newCommentDisplay);
+                }
             }
         })
-
-        function signalement(id){
+    }
+    //FORMULAIRE D'ENVOI DE COMMENTAIRE
+    $('.submit-btn').on('click', function(e){
+        e.preventDefault();
+        if($('#formCommentaire')[0].checkValidity()){
+            var idBillet = $('.post-info').attr('value');
+            var auteur = $('#auteur').val();
+            var contenu = $('#contenu').val();
             $.post({
-                 url: 'ajax',
-                 data: {'action': 'signalCom', 'idSignal' : id },
-                 success: function(data){
+                url:'ajax',
+                data:{'action': 'insertCom',
+                      'auteur': auteur,
+                      'contenu': contenu,
+                      'billetId': idBillet},
+                success:function(data){
+                    $('#formCommentaire')[0].reset(); 
                     showComment();
                 }
-            });
-        }  
-        //BOUTON SIGNALER
-        
-    
-    $(window).bind('load', function(){
+            })   
+        }
+    })
 
-        $('.signalbtn').on('click', function(){
-            signalement($(this).attr('value'));
-        });
-
-         let login = new Modal(document.querySelector('body'), {
-            id: 'connexion',
-            titre: 'Connexion',
-            type: 'connexion',
-            pseudonyme: '',
-            motDePasse: '',
-            confirmation: 'Veuillez rentrer vos identifiants'
-         });
-         $('#login').on('click', function(){
-             console.log('test connexion')
-             login;
-          });
-
-          
-            $('#connexion-validBtn').on('click', function(){
-                var username = $('#username').val();
-                var password = $('#password').val();
-                console.log('test');
-                $.post({
-                    url: 'login',
-                    data: {'action': 'login', 'username': username, 'password': password},
-                    success: function(data){
-                        console.log(data);
-                    }
-
-                });  
-            })
-    }) 
-
-    showComment();
-
-
-
-
-
+function signalement(id){
+    $.post({
+         url: 'ajax',
+         data: {'action': 'signalCom', 'idSignal' : id },
+         success: function(data){
+            showComment();
+        }
+    });
+}  
+    //BOUTON SIGNALER
     
 
-    
+$(window).bind('load', function(){
+    $('.signalbtn').on('click', function(){
+        signalement($(this).attr('value'));
+    });
+     let login = new Modal(document.querySelector('body'), {
+        id: 'connexion',
+        titre: 'Connexion',
+        type: 'connexion',
+        pseudonyme: '',
+        motDePasse: '',
+        confirmation: 'Veuillez rentrer vos identifiants'
+     });
+     $('#login').on('click', function(){
+         console.log('test connexion')
+         login;
+      });
+      
+        $('#connexion-validBtn').on('click', function(){
+            var username = $('#username').val();
+            var password = $('#password').val();
+            console.log('test');
+            $.post({
+                url: 'login',
+                data: {'action': 'login', 'username': username, 'password': password},
+                success: function(data){
+                    console.log(data);
+                }
+            });  
+        })
+}) 
+
+billetAccueil()
+pagination();
+showComment();
+
