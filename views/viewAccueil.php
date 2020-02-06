cc1259c23229c4b610b7eb513d457e4fee48b868
LudovicFavory <lu.favory@gmail.com>
2020-02-06 14:57:36 +0100

    Update viewAccueil.php

--- views/viewAccueil.php
+++ views/viewAccueil.php
@@ -5,34 +5,11 @@
 	<section class="blog-area section">
 		<div class="container">
 
-			<div class="row">
-
-        <?php
-        foreach ($billets as $billet):
-         ?>
-
-
-				<div class="col-lg-4 col-md-6">
-					<div class="card h-100">
-						<div class="single-post post-style-1">
-
-							<div class="blog-image"><img src="public/images/marion-michele-330691.jpg" alt="Blog Image"></div>
-							
-							<div class="blog-info">
-
-								<h4 class="title"><a href="post&id=<?= $billet->id() ?>"><b><?= $billet->titre() ?></b></a></h4>
-
-
-							</div><!-- blog-info -->
-						</div><!-- single-post -->
-					</div><!-- card -->
-				</div><!-- col-lg-4 col-md-6 -->
-
-        <?php endforeach ?>
+			<div class="row" id="billetAccueil">
 
 			</div><!-- row -->
 
-			<a class="load-more-btn" href="#"><b>LOAD MORE</b></a>
+			<div id='paginationAccueil'></div>
 
 		</div><!-- container -->
 	</section><!-- section -->
