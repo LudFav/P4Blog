eed1c7a839f7837482e3844f3c699a1ea3343c22
LudFav <45117462+LudFav@users.noreply.github.com>
2020-02-09 17:45:24 +0100

    Update ControllerLogin.php

--- controllers/ControllerLogin.php
+++ controllers/ControllerLogin.php
@@ -7,35 +7,47 @@ class ControllerLogin {
   
     public function __construct(){
   
-    $this->_userManager = new UserManager;
-    $this->login(); 
+    $this->_userManager = new UserManager; 
+    $this->login();
+    $this->isLoggedIn();
+   // $this->logout();
+    
   }
-    
-  public function login() {
-    
-    $userInfo = $this->_userManager->getUser();
-    session_start();
-    //$passToHash = password_hash('admin', PASSWORD_DEFAULT, ['cost' => 12]);
-    if(isset($_POST['username']) && isset$_POST['password']){
-      echo ' test PHP';
-      $username = htmlspecialchars($_POST['username']);
-     
-      $passwordSubmitted = htmlspecialchars($_POST['password']);
-      $passwordHashed = $userInfo[0]->password();
-      $password= password_verify($passwordSubmitted, $passwordHashed);
-     
-      if($_POST['username'] == $userInfo[0]->username() && $password == true){
-        $_SESSION['admin'] = $username;
-        var_dump($_SESSION['admin']);
-        echo " Bienvenue $username";
-      } else {
-        echo "Erreur : Erreur de login </br>";
+  public function login(){
+    if(isset($_POST['action']) && $_POST['action'] == 'login'){  
+      $userInfo = $this->_userManager->getUser();
+      //$passToHash = password_hash('admin', PASSWORD_DEFAULT, ['cost' => 12]);
+      if(isset($_POST['username']) && isset($_POST['password'])){
+        $username = htmlspecialchars($_POST['username']);
+      
+        $passwordSubmitted = htmlspecialchars($_POST['password']);
+        $passwordHashed = $userInfo[0]->password();
+        $password= password_verify($passwordSubmitted, $passwordHashed);
+      
+        if($_POST['username'] == $userInfo[0]->username() && $password == true){
+          $_SESSION['admin'] = $username;
+        } else {
+          echo "Erreur : Erreur de login </br>";
+        }
       }
     }
   }
-
-  public function logout() {
-
+public function isLoggedIn(){
+  if(isset($_SESSION['admin']) && !empty($_SESSION['admin'])){
+    //$loginOutput='';
+    //$loginOutput .= '<li><a href="admin">Administration</a></li>';
+    //$data['loginOutput'] = $loginOutput;
+    print 'Active';
+    //exit($data['loginOutput']);
   }
+}
+  public function logout(){
+  if(isset($_POST['action']) && $_POST['action'] == 'logout'){
+    unset($_SESSION['admin']);   
+    session_destroy();
+    echo "window.location.href='accueil'";
+    exit;
+  }
+}
 } 
 ?>
