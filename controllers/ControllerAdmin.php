7e4b99b31adc3040d269f40905717546409a4500
LudFav <45117462+LudFav@users.noreply.github.com>
2020-02-09 15:14:12 +0100

    Update ControllerAdmin.php

--- controllers/ControllerAdmin.php
+++ controllers/ControllerAdmin.php
@@ -7,11 +7,14 @@ class ControllerAdmin{
     private $_view;
     public function __construct(){
       //VUE
-      $this->_view = new View('Admin');
+      $this->_view = new View('Admin'); 
       if(isset($_SESSION['admin']) && !empty($_SESSION['admin'])){ 
-      $this->_view->generate(array());
+      echo ' test session ok';
+      $this->_view->generateAdmin(array());
       } else {
+        $this->_view->generate(array());
+        echo ' test session non ok';
         header('Location:accueil'); 
-      }  
+      }
     }
 }
