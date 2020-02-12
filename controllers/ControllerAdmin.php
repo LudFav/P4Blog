9014fb74659afba5ff38dd4f9e78aac6c622a8e8
LudovicFavory <lu.favory@gmail.com>
2020-02-12 15:47:32 +0100

    Update ControllerAdmin.php

--- controllers/ControllerAdmin.php
+++ controllers/ControllerAdmin.php
@@ -1,12 +1,16 @@
 <?php
-
+require_once 'controllers/ajaxAdminPhp/ajaxAdminBillet.php';
+require_once 'controllers/ajaxAdminPhp/ajaxAdminComSign.php';
+require_once 'controllers/ajaxAdminPhp/ajaxAdminComMod.php';
+require_once 'models/CommentManager.php';
 require_once 'views/View.php';
 
 class ControllerAdmin{
-    
+    private $_billetManager;
     private $_view;
     public function __construct(){
       if(isset($_SESSION['admin']) && !empty($_SESSION['admin'])){
+        
         $this->_view = new View('Admin'); 
         $this->_view->generateAdmin(array());  
     } else {
