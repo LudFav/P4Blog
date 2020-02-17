c6a8d5280293757ecf64e9a5143975c76d9f5681
LudovicFavory <lu.favory@gmail.com>
2020-02-17 17:00:13 +0100

    Update ajaxAdminBillet.php

--- controllers/ajaxAdminPhp/ajaxAdminBillet.php
+++ controllers/ajaxAdminPhp/ajaxAdminBillet.php
@@ -5,13 +5,12 @@ spl_autoload_register(function($class){
 });
 
 $_billetManager;
-
-
-
 $_billetManager = new BilletManager;  
 isset($_POST['page']) && is_numeric($_POST['page'])? $page = $_POST['page'] : $page = 1;
 $entiteParPage = 4; 
 $billets = $_billetManager->getBillets($page, $entiteParPage);
+ $nbreEntitesParPage = $entiteParPage;
+    $pages = $_billetManager->getPageMax($nbreEntitesParPage);
 if(isset($_POST['action']) && $_POST['action']=='showbillet'){
     $billetsOutput = '';
     foreach ($billets as $billet){ 
@@ -20,7 +19,7 @@ if(isset($_POST['action']) && $_POST['action']=='showbillet'){
          $billetsOutput.= '<td>' .$billet->auteur(). '</td>';
          $billetsOutput.= '<td>' .$billet->titre(). '</td>';
          $billetsOutput.= '<td>' .$billet->date(). '</td>';
-         $billetsOutput.= '<td class="actionTd">';
+         $billetsOutput.= '<td class="actionTd" value="' .$pages. '">';
          $billetsOutput.= '<button class="visualBilBtn"><a href="post&id=' .$billet->id(). '" ><i class="fa fa-eye"></i></a></button>';
          $billetsOutput.= '<button class="editBilBtn" ><a href="update&id=' .$billet->id(). '"><i class="fa fa-pencil" aria-hidden="true"></i></a></button>';
          $billetsOutput.= '<button class="deleteBilBtn" value="' .$billet->id(). '" data-toggle="modal" data-target ="#deleteBillet" ><i class="fa fa-trash" aria-hidden="true"></i></button>';
