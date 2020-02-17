2ca1e687983e8181e5d8211f18fc8644724a2b3a
LudovicFavory <lu.favory@gmail.com>
2020-02-17 17:00:33 +0100

    Update ControllerPost.php

--- controllers/ControllerPost.php
+++ controllers/ControllerPost.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'views/View.php';
-require_once 'controllers/ajaxClientPhp/ajaxCom.php';
+require_once 'controllers/ajaxClientPhp/ajaxClientCom.php';
 
 class ControllerPost {
   private $_billetManager;
