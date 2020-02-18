383d9994a7a26c8502f646b7b059df4a329f2620
LudovicFavory <lu.favory@gmail.com>
2020-02-18 11:55:49 +0100

    Update Pagination.js

--- assets/js/Pagination.js
+++ assets/js/Pagination.js
@@ -3,47 +3,87 @@ class Pagination {
      * @param {HTMLElement} element
      * @param {Object} options
      *  @param {string} id
-     *  @param {int} page
-     *  @param {int} maxPages
+     *  @param {string} urlAjax
      * @param {int} pageNav
     **/
     constructor(element, options) {
         this.element = element;
         this.options = {
             id: options.id,
-            page: options.page,
-            maxPages: options.maxPages,
-            pageNav: options.pageNav,
+            urlAjax: options.urlAjax,
+            pageNav: options.pageNav
         };
-        this.paginate();
+        
+        this.paginationAjax();
     }
-        paginate() {
-            
+
+        paginationAjax(){
+            $.post({
+                url: this.options.urlAjax,
+                data:{'action': 'pagination', 'page': 2},
+                success: (data) =>{
+                    pagination = JSON.parse(data);
+                    let adminPage = parseInt(pagination.page);
+                    let adminMaxPages = pagination.maxPages;
+                    console.log('adminPage ='+adminPage)
+                    console.log('adminMaxPages ='+adminMaxPages)
+                    this.paginate(adminPage, adminMaxPages);
+                }
+            })
+        }
+
+        paginate(adminPage, adminMaxPages) {
+           
             let paginationId = this.options.id;
             
             this.pagination = $('<nav aria-label="Page navigation '+paginationId+'"></nav>').appendTo(this.element);  
             this.pagination.attr('id', paginationId);
-            this.paginationUl = $('<ul class="paginationUl" value="'+this.options.pageNav+'"></ul>').appendTo(this.pagination);
+            this.paginationUl = $('<ul class="paginationUl"></ul>').appendTo(this.pagination);
             this.paginationPrev = $('<li class="page-item"><button class="'+paginationId+' page-link prev">Previous</button></li>').appendTo(this.paginationUl); 
-
-            /*for(let i = this.options.page - this.options.pageNav; i < this.options.page; i++){
+            
+            /*for(let i = adminPage - adminPageNav; i < adminPage; i++){
                 if(i> 0){
                 
                  this.leftPageNav= $('<li class="page-item"><a class="'+paginationId+' page-link" value='+i+'>' +i+ '</a></li>').appendTo(this.paginationUl);
                 }
                
             }*/
-            this.currentPage= $('<li class="page-item"><p class=" '+paginationId+' page-link active" value="1"></p></li>').appendTo(this.paginationUl);
-            /*for(let j = this.options.page +1; j <= this.options.maxPages; j++){
+            console.log(adminPage + 1);
+            this.currentPage= $('<li class="page-item"><p class=" '+paginationId+' page-link active" value="'+adminPage+'">'+adminPage+'</p></li>').appendTo(this.paginationUl);
+            /*for(let j = adminPage +1; j <= adminMaxPages; j++){
                 this.rightPageNav= $('<li class="page-item"><a class=" '+paginationId+' page-link" value='+j+'>'+j+'</a></li>').appendTo(this.paginationUl);
                 
-                if(j >= this.options.page + this.options.pageNav){
+                if(j >= adminPage + adminPageNav){
                 break;
                 }
             } */
+       
             this.paginationNext = $('<li class="page-item"><button class=" '+paginationId+' page-link next">Next</button></li>').appendTo(this.paginationUl);
-               
-        
+            this.paginationNext.on('click', function() {
+                console.log('test Next')
+                currentPageValue++;
+            }); 
+            
+            $('.page-link.prev').on('click', function() {
+                console.log('test Prev')   
+                currentPageValue--;
+            });  
+
+            let currentPageValue = $('.page-link.active').attr('value');
+
+            $(currentPageValue).on('change', function(){
+                $('.page-link.active').text(currentPageValue);
+                if($(currentPageValue) <= 1 ){
+                    this.paginationPrev.hide();
+                }else {
+                    this.paginationPrev.show();
+                }
+                if($(currentPageValue)>= adminMaxPages){
+                    this.paginationNext.hide();
+                } else {
+                    this.paginationNext.show();
+                }
+            })  
 
           /* $('.'+paginationId+'.page-link').on('click', function(){
                 let num = $(this).attr('value');
@@ -54,4 +94,7 @@ class Pagination {
             }); 
          */
         } 
+     
+
+       
 }
