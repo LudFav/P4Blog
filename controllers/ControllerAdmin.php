7436a32b27fa4705d693e9534c40c94371ef457f
LudovicFavory <lu.favory@gmail.com>
2020-02-19 14:28:18 +0100

    Update ControllerAdmin.php

--- controllers/ControllerAdmin.php
+++ controllers/ControllerAdmin.php
@@ -10,7 +10,6 @@ class ControllerAdmin{
     private $_view;
     public function __construct(){
       if(isset($_SESSION['admin']) && !empty($_SESSION['admin'])){
-        
         $this->_view = new View('Admin'); 
         $this->_view->generateAdmin(array());  
     } else {
