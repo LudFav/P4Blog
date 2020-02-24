90dede892d5d2ad4c614762192607b86abe92322
LudovicFavory <lu.favory@gmail.com>
2020-02-24 16:06:12 +0100

    Update viewError.php

--- views/viewError.php
+++ views/viewError.php
@@ -1,1 +1,5 @@
-<h1>ERREUR 404</h1>
+<div class="alert alert-primary" role="alert" style="height:100%; background: url(assets/images/errorImg.jpg) no-repeat center center">
+<h1 class="errorMessage titre">ERREUR 404</h1>
+<p class="errorMessage text">L'url renseignée n'existe pas, veuillez cliquer sur le lien ci-dessous pour revenir à la page d'accueil</p>
+<button class ="btn btn-success errorMessage"><a href="accueil">Accueil</a></button>
+</div>
