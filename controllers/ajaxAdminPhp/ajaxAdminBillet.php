d18b0f591c965270f5a3e54d63cf45bc2d463787
LudovicFavory <lu.favory@gmail.com>
2020-02-24 17:05:17 +0100

    Update ajaxAdminBillet.php

--- controllers/ajaxAdminPhp/ajaxAdminBillet.php
+++ controllers/ajaxAdminPhp/ajaxAdminBillet.php
@@ -3,7 +3,7 @@
 spl_autoload_register(function($class){
     require_once($_SERVER["DOCUMENT_ROOT"].'/PROJET4-MVC-OOP-PHP/models/'.$class.'.php');
 });
-echo $_SERVER["DOCUMENT_ROOT"].'/PROJET4';
+
 $_billetManager;
 $_billetManager = new BilletManager; 
 
@@ -14,7 +14,7 @@ $page = isset($_POST['page'])? $_POST['page'] :1;
 
 $pageDebillets = $_billetManager->getBillets($page, $entiteParPage);
 $pages = $_billetManager->getPageMax($nbreEntitesParPage);
-
+echo $pagedeBillets;
 //TABLEAU BILLET ET PAGE
 if(isset($_POST['action']) && $_POST['action']=='showbillet'){
     $billetsOutput = '';
