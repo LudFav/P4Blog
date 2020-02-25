a3c998c7b6b3bdab1ba9ebd220b7404750de7f4d
LudovicFavory <lu.favory@gmail.com>
2020-02-25 17:04:29 +0100

    Create viewSommaire.php

---
+++ views/viewSommaire.php
@@ -0,0 +1,21 @@
+
+<section class="blog-area section">
+
+    <div id="bg"></div>
+    
+		<div class="container">
+        <nav>
+		<ul>
+			<li><a href="accueil" class="fas fa-book" id="accueil"></a><span class="label"></span></a></li>
+			<li><a href="" class="fas fa-sign-in-alt" id="login" data-toggle="modal" data-target="#connexion"><span class="label"></span></a></li>
+		</ul>
+        </nav>
+        <h1>Billet simple pour l'Alaska</h1>
+			<div class="row" id="billetAccueil">
+
+			</div><!-- row -->
+
+			<div id='paginationAccueil'></div>
+		
+		</div><!-- container -->
+</section><!-- section -->
