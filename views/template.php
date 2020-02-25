4ac23db3886ab17a281806c9b2f41c9c6552bb38
LudovicFavory <lu.favory@gmail.com>
2020-02-25 17:04:20 +0100

    Update template.php

--- views/template.php
+++ views/template.php
@@ -10,44 +10,27 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
+		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
+		<link href="assets/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+		<!-- Stylesheets -->
+
+		<link href="public/common-css/bootstrap.css" rel="stylesheet">
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 	</head>
-	<body class="is-preload">
-		<div id="wrapper">
-			<div id="bg"></div>
-			<div id="overlay"></div>
-			<div id="main">
-
-				<!-- Header -->
-					<header id="header">
-						<h1>Billet simple pour l'Alaska</h1>
-						<p>de Jean Forteroche</p>
-						<nav>
-							<ul>
-								<li><a href="accueil" class="fas fa-book"></a><span class="label"></span></a></li>
-								<li><a href="" class="fas fa-sign-in-alt"><span class="label">Facebook</span></a></li>
-							</ul>
-						</nav>
-					</header>
-					<?= $content ?>
-				<!-- Footer -->
-					<footer id="footer">
-						<span class="copyright">&copy; Untitled. Design: <a href="http://html5up.net">HTML5 UP</a>.</span>
-					</footer>
-
-			</div>
-		</div>
-		<script>
+	<body>
+		<?= $content ?>	
+			<script>
 			window.onload = function() { document.body.classList.remove('is-preload'); }
-			window.ontouchmove = function() { return false; }
-			window.onorientationchange = function() { document.body.scrollTop = 0; }
-		</script>
-		<script src="public/common-js/jquery-3.1.1.min.js"></script>
-		<script src="public/common-js/tether.min.js"></script>
-		<script src="public/common-js/bootstrap.js"></script>
-		<script src="public/common-js/scripts.js"></script>
-		<script src="assets/js/Modal.js"></script>
-		<script src="assets/js/Pagination.js"></script>
-		<script src="assets/js/ajax.js"></script>
+				window.ontouchmove = function() { return false; }
+				window.onorientationchange = function() { document.body.scrollTop = 0; }
+			</script>
+			<script src="public/common-js/jquery-3.1.1.min.js"></script>
+			<script src="public/common-js/tether.min.js"></script>
+			<script src="public/common-js/bootstrap.js"></script>
+			<script src="public/common-js/scripts.js"></script>
+			<script src="assets/js/Modal.js"></script>
+			<script src="assets/js/Pagination.js"></script>
+			<script src="assets/js/ajax.js"></script>
 	</body>
 </html>
