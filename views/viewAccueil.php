7eaa5ec014d613e9eaeafb8c33fb02d170eb267a
LudovicFavory <lu.favory@gmail.com>
2020-02-26 16:19:12 +0100

    Update viewAccueil.php

--- views/viewAccueil.php
+++ views/viewAccueil.php
@@ -1,24 +1,17 @@
-<div id="wrapper">
-	<nav>
-		<ul>
-			<li><a href="sommaire" class="fas fa-book" id="sommaire"></a><span class="label"></span></a></li>
-			<li><a href="" class="fas fa-sign-in-alt" id="login" data-toggle="modal" data-target="#connexion"><span class="label"></span></a></li>
-		</ul>
-	</nav>
-			
+<!--<div id="wrapper">			
 	<div id="bg"></div>
 
 			<div id="overlay"></div>
-			<div id="main">
-				<header id="header">
+			<div id="main">-->
+				<header id="headerAcueil">
 					<h1>Billet simple pour l'Alaska</h1>
 					<p>de Jean Forteroche</p>
-					<nav>
+					<!--<nav>
 						<ul>
 							<li><a href="sommaire" class="fas fa-book" id="sommaire"></a><span class="label"></span></a></li>
 							<li><a href="" class="fas fa-sign-in-alt" id="login" data-toggle="modal" data-target="#connexion"><span class="label"></span></a></li>
 						</ul>
-					</nav>
+					</nav>-->
 				</header>
 				
 				<footer id="footer">
