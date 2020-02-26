96e19c86a99f1f54b3aaf94918c5a3e7528d2a69
LudovicFavory <lu.favory@gmail.com>
2020-02-26 16:19:04 +0100

    Update viewSommaire.php

--- views/viewSommaire.php
+++ views/viewSommaire.php
@@ -1,21 +1,13 @@
 
 <section class="blog-area section">
-
-    <div id="bg"></div>
-    
-		<div class="container">
-        <nav>
-		<ul>
-			<li><a href="accueil" class="fas fa-book" id="accueil"></a><span class="label"></span></a></li>
-			<li><a href="" class="fas fa-sign-in-alt" id="login" data-toggle="modal" data-target="#connexion"><span class="label"></span></a></li>
-		</ul>
-        </nav>
-        <h1>Billet simple pour l'Alaska</h1>
+    <div class="container">
+        <h1>Sommaire</h1>
 			<div class="row" id="billetAccueil">
 
-			</div><!-- row -->
+			</div><!-- .row -->
 
 			<div id='paginationAccueil'></div>
 		
-		</div><!-- container -->
-</section><!-- section -->
+    </div><!-- .container -->
+    </div>><!--#wrapper dans template -->
+</section><!-- .section -->
