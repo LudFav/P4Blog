9e35f4d5b941fffee83e353e7cb507b7484eee02
LudovicFavory <lu.favory@gmail.com>
2020-02-26 17:01:10 +0100

    Update viewUpdate.php

--- views/viewUpdate.php
+++ views/viewUpdate.php
@@ -3,7 +3,7 @@
 
   <div class="form-group">
     <label for="titre" class="labelBillet"><b>Titre</b></label>
-    <input type="text" name="titre" class="form-control" id="titreB" value="<?=$billet[0]->titre()?>">
+    <input type="text" name="titre" class="form-control" id="titreB" value="<?=$billet[0]->titre()?>" required>
   </div>
 
   <div class="form-group">
@@ -11,5 +11,5 @@
     <textarea class="form-control" name="contenu" id="textareaC" rows="3"><?=$billet[0]->contenu()?></textarea>
   </div>
 
-  <input name="updateBillet" type="submit" value="valider" class="addBillet"/>
+  <input name="updateBillet" type="submit" value="valider" class="btn-success addBillet"/>
 </form>
