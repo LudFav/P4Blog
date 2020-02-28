acc1f4021610b50adfed510e658983a223787893
LudFav <45117462+LudFav@users.noreply.github.com>
2020-02-28 09:20:15 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,9 @@
 $(document).ready(function() {
-	/*const queryString = document.URL;
-	console.log(queryString);
-
-	const urlParams = new URLSearchParams(queryString);
-
-	const blabla = urlParams.has('post&id');
-	console.log(blabla);*/
-	var url_param       = $(location).attr('href').split("/");
-	console.log(url_param)
-	var param           = (url_param[4]).split("&")
-	var whatIwant = (param[0]);
-	console.log(whatIwant)
-	if(whatIwant === 'sommaire'){
+	var url_param = $(location).attr('href').split("/");
+	var param = (url_param[4]).split("&")
+	var shortUrl = (param[0]);
+	if(shortUrl === 'sommaire'){
 		let accueil = '<a href="accueil" class="fas fa-angle-left" id="accueil"></a><span class="label"></span></a>';  
 		$('#retoutSommaire').replaceWith($(accueil));
 	}
 });
-
-
-
