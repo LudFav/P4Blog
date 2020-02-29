6e5232d0420f36990ad8f104caab3de178a45351
LudFav <45117462+LudFav@users.noreply.github.com>
2020-02-29 15:41:30 +0100

    Update ControllerSommaire.php

--- controllers/ControllerSommaire.php
+++ controllers/ControllerSommaire.php
@@ -10,11 +10,8 @@ class ControllerSommaire{
       throw new \Exception("Page introuvable", 1);
     } else {
       $this->_view = new View('Sommaire');
-      if(isset($_SESSION['admin']) && !empty($_SESSION['admin'])){
-      $this->_view->generateAdmin(array());
-      } else {
-        $this->_view->generate(array());
-      }
+      $this->_view->generate(array());
     } 
-  }
+  } 
 }
+
