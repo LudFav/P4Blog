6eac50ae850304d278e7930c401b51015e46f9ed
LudFav <45117462+LudFav@users.noreply.github.com>
2020-02-29 17:08:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-// instanciation du routeur et on utilise la methode routeReq qui va choisir le bon controller selon l'url
 
 require_once($_SERVER["DOCUMENT_ROOT"]. '/P4Blog/controllers/Router.php');
 
