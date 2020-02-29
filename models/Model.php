772cd14656c77b6b867da29145f55ce700d68b68
LudFav <45117462+LudFav@users.noreply.github.com>
2020-02-29 15:47:18 +0100

    Update Model.php

--- models/Model.php
+++ models/Model.php
@@ -4,8 +4,9 @@ abstract class Model{
   protected static $_bdd;
 //connexion a la bdd
   private static function setBdd(){
-    self::$_bdd = new PDO('mysql:host=db5000307985.hosting-data.io;dbname=dbs300525;charset=utf8', 'dbu539508', 'Open%Class*R00m');
-//on utilise les constantes de PDO pour gérer les erreurs
+    //self::$_bdd = new PDO('mysql:host=db5000307985.hosting-data.io;dbname=dbs300525;charset=utf8', 'dbu539508', 'Open%Class*R00m');
+    self::$_bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+    //on utilise les constantes de PDO pour gérer les erreurs
     self::$_bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
   }
 //fonction de connexion par defaut a la bdd
