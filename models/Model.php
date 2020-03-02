6655fc9c839b2a84eb56c10618872e410966c30c
LudovicFavory <lu.favory@gmail.com>
2020-03-02 08:47:58 +0100

    Update Model.php

--- models/Model.php
+++ models/Model.php
@@ -8,7 +8,7 @@ abstract class Model
   //connexion a la bdd
 
   private static function setBdd(){
-    self::$_bdd = new PDO('mysql:localhost;dbname=blog;charset=utf8', 'root', '');
+    self::$_bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
     //self::$_bdd = new PDO('mysql:host=db5000307985.hosting-data.io;dbname=dbs300525;charset=utf8', 'dbu539508', 'Open%Class*R00m');
     //on utilise les constantes de PDO pour gérer les erreurs
     self::$_bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
