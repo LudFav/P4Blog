e5fa9a7a3e905bc4cf4c6f91690b40668488b942
LudovicFavory <lu.favory@gmail.com>
2020-03-02 11:39:40 +0100

    Update templateAdmin.php

--- views/templateAdmin.php
+++ views/templateAdmin.php
@@ -6,7 +6,7 @@
 -->
 <html lang="fr">
 	<head>
-		<title>Billet simple pour l'Alaska</title>
+		<title><?= $title?></title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link href="assets/css/main.css" rel="stylesheet"/>
