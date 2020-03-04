b0ed8368eb6a23a8d2d275d813975563cc8552a4
LudovicFavory <lu.favory@gmail.com>
2020-03-04 10:09:14 +0100

    Update ControllerAccueil.php

--- controllers/ControllerAccueil.php
+++ controllers/ControllerAccueil.php
@@ -1,7 +1,5 @@
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"]. '/P4Blog/views/View.php';
-require_once $_SERVER["DOCUMENT_ROOT"]. '/P4Blog/controllers/ajaxClientPhp/ajaxClientBillet.php';
-require_once $_SERVER["DOCUMENT_ROOT"]. '/P4Blog/controllers/ajaxClientPhp/ajaxClientCom.php';
 
 class ControllerAccueil{
   private $_view;
@@ -10,11 +8,7 @@ class ControllerAccueil{
       throw new \Exception("Page introuvable", 1);
     } else {
       $this->_view = new View('Accueil');
-      if(isset($_SESSION['admin']) && !empty($_SESSION['admin'])){
-      $this->_view->generate("Billet simple pour l'Alaska", array());
-      } else {
-        $this->_view->generate("Billet simple pour l'Alaska", array());
-      }
+      $this->_view->generate("Billet simple pour l'Alaska", array()); 
     } 
   }
 }
